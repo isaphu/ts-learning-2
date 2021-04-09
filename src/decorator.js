cbdf003a10b1c9c825920fba85dec0e3499d2cf2
isaphu <uronohan@gmail.com>
2021-04-09 15:32:00 +0700

    update generics

--- src/decorator.js
+++ src/decorator.js
@@ -1,33 +1,36 @@
-// function Logger(logString: string) {
-//   return function (constructor: Function) {
-//     console.log(logString);
-//     console.log(constructor);
-//   };
-// }
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function WithTemplate(template, hookId) {
+function Logger(logString) {
+    console.log('LOGGER FACTORY');
     return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+function WithTemplate(template, hookId) {
+    console.log('TEMPLATE FACTORY');
+    return function (constructor) {
+        console.log('Rendering template');
         var hookEl = document.getElementById(hookId);
         var p = new constructor();
         if (hookEl) {
             hookEl.innerHTML = template;
-            hookEl.querySelector('h1').textContent = p.name;
+            hookEl.querySelector("h1").textContent = p.name;
         }
     };
 }
 //@ is a pointer to the function
-// @Logger("LOGGING - PERSON")
 var Person = /** @class */ (function () {
     function Person() {
-        this.name = "Isa";
+        this.name = ["Isa"];
         console.log("Creating person object...");
     }
     Person = __decorate([
+        Logger("LOGGING - PERSON"),
         WithTemplate("<h1>My Person Object</h2>", "app")
     ], Person);
     return Person;
--- src/decorator.ts
+++ src/decorator.ts
@@ -1,26 +1,29 @@
-// function Logger(logString: string) {
-//   return function (constructor: Function) {
-//     console.log(logString);
-//     console.log(constructor);
-//   };
-// }
+function Logger(logString: string) {
+    console.log('LOGGER FACTORY')
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
 
 function WithTemplate(template: string, hookId: string) {
-  return function(constructor: any) {
+    console.log('TEMPLATE FACTORY')
+  return function (constructor: any) {
+      console.log('Rendering template')
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
       hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
   };
 }
-  
+
 //@ is a pointer to the function
-// @Logger("LOGGING - PERSON")
+@Logger("LOGGING - PERSON")
 @WithTemplate("<h1>My Person Object</h2>", "app")
-class Person {
-  name = "Isa";
+class Person{
+  name = ["Isa"];
 
   constructor() {
     console.log("Creating person object...");
