90ec292421ee8895847f6ea590fa47d3b421d0b7
isaphu <uronohan@gmail.com>
2021-04-09 16:35:35 +0700

    decorator and class

--- src/decorator.js
+++ src/decorator.js
@@ -1,32 +1,61 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 function Logger(logString) {
-    console.log('LOGGER FACTORY');
+    console.log("LOGGER FACTORY");
     return function (constructor) {
         console.log(logString);
         console.log(constructor);
     };
 }
 function WithTemplate(template, hookId) {
-    console.log('TEMPLATE FACTORY');
-    return function (constructor) {
-        console.log('Rendering template');
-        var hookEl = document.getElementById(hookId);
-        var p = new constructor();
-        if (hookEl) {
-            hookEl.innerHTML = template;
-            hookEl.querySelector("h1").textContent = p.name;
-        }
+    console.log("TEMPLATE FACTORY");
+    return function (originalConstructor) {
+        return /** @class */ (function (_super) {
+            __extends(class_1, _super);
+            function class_1() {
+                var _ = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    _[_i] = arguments[_i];
+                }
+                var _this = _super.call(this) || this;
+                console.log("Rendering template");
+                var hookEl = document.getElementById(hookId);
+                if (hookEl) {
+                    hookEl.innerHTML = template;
+                    hookEl.querySelector("h1").textContent = _this.name;
+                }
+                return _this;
+            }
+            return class_1;
+        }(originalConstructor));
     };
 }
 //@ is a pointer to the function
 var Person = /** @class */ (function () {
     function Person() {
-        this.name = ["Isa"];
+        this.name = "Isa";
         console.log("Creating person object...");
     }
     Person = __decorate([
@@ -37,3 +66,56 @@ var Person = /** @class */ (function () {
 }());
 var pers = new Person();
 console.log(pers);
+function Log(target, propertyName) {
+    console.log("Porperty decorator");
+    console.log(target, propertyName);
+}
+function Log2(target, name, descriptor) {
+    console.log("Access decorator!");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log3(target, name, descriptor) {
+    console.log("Method decorator!");
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+function Log4(target, name, position) {
+    console.log("Parameter decorator!");
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+var Product = /** @class */ (function () {
+    function Product(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    Object.defineProperty(Product.prototype, "price", {
+        set: function (val) {
+            if (val > 0) {
+            }
+            else {
+                throw new Error("Invalid price - shouldbe positive!");
+            }
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Product.prototype.getPriceWithTax = function (tax) {
+        return this.price * (1 + tax);
+    };
+    __decorate([
+        Log
+    ], Product.prototype, "title");
+    __decorate([
+        Log2
+    ], Product.prototype, "price");
+    __decorate([
+        Log3,
+        __param(0, Log4)
+    ], Product.prototype, "getPriceWithTax");
+    return Product;
+}());
--- src/decorator.ts
+++ src/decorator.ts
@@ -1,5 +1,5 @@
 function Logger(logString: string) {
-    console.log('LOGGER FACTORY')
+  console.log("LOGGER FACTORY");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -7,23 +7,28 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-    console.log('TEMPLATE FACTORY')
-  return function (constructor: any) {
-      console.log('Rendering template')
-    const hookEl = document.getElementById(hookId);
-    const p = new constructor();
-    if (hookEl) {
-      hookEl.innerHTML = template;
-      hookEl.querySelector("h1")!.textContent = p.name;
-    }
+  console.log("TEMPLATE FACTORY");
+  return function<T extends {new(...args: any[]): {name:string}} >(
+      originalConstructor: T) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
   };
 }
 
 //@ is a pointer to the function
 @Logger("LOGGING - PERSON")
 @WithTemplate("<h1>My Person Object</h2>", "app")
-class Person{
-  name = ["Isa"];
+class Person {
+  name = "Isa";
 
   constructor() {
     console.log("Creating person object...");
@@ -32,3 +37,57 @@ class Person{
 
 const pers = new Person();
 console.log(pers);
+
+function Log(target: any, propertyName: string | symbol) {
+  console.log("Porperty decorator");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Access decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+    } else {
+      throw new Error("Invalid price - shouldbe positive!");
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this.price * (1 + tax);
+  }
+}
